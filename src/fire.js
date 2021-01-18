7d5fbdb70069758776b517a662e00adceb518680
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2021-01-18 03:10:57 +0000

    Added firebase hosting

--- src/fire.js
+++ src/fire.js
@@ -3,6 +3,7 @@ import firebase from 'firebase';
 var firebaseConfig = {
     apiKey: "AIzaSyAl5AAte7Pp9tWwHh_0UG0I1ouHgZCubnU",
     authDomain: "fulguritchallenge.firebaseapp.com",
+    databaseURL: "https://fulguritchallenge-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "fulguritchallenge",
     storageBucket: "fulguritchallenge.appspot.com",
     messagingSenderId: "40595159459",
